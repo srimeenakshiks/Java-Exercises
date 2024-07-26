d9f5ac3c04e75e2880f8655a07ac4cd65eef257c
Srimeenakshi KS <meenakshi.ks2022@vitstudent.ac.in>
2024-07-26 09:54:03 +0530

    Create tuition_fee_calculation.java
    Prompt the user to enter the tuition fee and find the number of years after which the fee will be doubled, given that the tuition fee increases by 7% every year.

---
+++ tuition_fee_calculation.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class fee {
+public static void main(String args[]) { Scanner input = new Scanner(System.in); System.out.println("Tuition fee: "); Double n = input.nextDouble();
+int year = 0;
+double tuition_fee = n;
+while(tuition fee < n°2) {
+tuition_fee = tuition_fee + (0.07*tuition_fee);
+year++; }
+System.out.println("In " + year + " years, the tuition fee will be doubled and the value
+is " + tuition fee);
+}
