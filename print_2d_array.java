d4d3a3c895a692c04022b4e06b554614e8a5c983
Srimeenakshi KS <meenakshi.ks2022@vitstudent.ac.in>
2024-07-26 10:19:38 +0530

    Create print_2d_array.java
    Store and print a two-dimensional array.

---
+++ print_2d_array.java
@@ -0,0 +1,6 @@
+﻿import java.util.Scanner;
+public class Main {
+public static void main(String[] args) { Scanner input = new Scanner(System.in); System.out.println("number of rows: "); int r = input.nextInt();
+System.out.println("number of columns: "); int c = input.nextInt(); int[][] array = new int[r][c]; for(int i=0; i<r; i++){ for(int j=0; j<c; j++) { System.out.println("Enter: "); array[i][j] = input.nextInt();
+for (int i=0; i<r; i++) { for (int j=0; j < c; j++) { System.out.print(array[i][j] + " ");
+System.out.println();
