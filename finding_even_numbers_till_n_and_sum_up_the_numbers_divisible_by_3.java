727f2296b636ac8c5a25159fabea3395e28bef9a
Srimeenakshi KS <meenakshi.ks2022@vitstudent.ac.in>
2024-07-26 09:37:31 +0530

    Create finding_even_numbers_till_n_and_sum_up_the_numbers_divisible_by_3.java
    Prompt the user to input a value "n". Find all the even numbers till "n" and sum up all the numbers that are divisible by 3.

---
+++ finding_even_numbers_till_n_and_sum_up_the_numbers_divisible_by_3.java
@@ -0,0 +1,13 @@
+﻿import java.util.Scanner;
+public class hello_world {
+public static void main(String args[]) { Scanner input = new Scanner(System.in); System.out.println("n: ");
+Double n = input.nextDouble();
+double sum = 0;
+for(int i = 0; i < n+1; i++) {
+}
+if (1%2==0) {
+}
+if (i%3 == 0) {
+}
+sum += i;
+System.out.println("The sum is " + sum);
