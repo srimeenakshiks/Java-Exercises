dceddaa0549dcb0cb7f9f71f35004cfc05402595
Srimeenakshi KS <meenakshi.ks2022@vitstudent.ac.in>
2024-07-26 09:58:06 +0530

    Create store_and_print_array.java
    Store and print an array of integers.

---
+++ store_and_print_array.java
@@ -0,0 +1,8 @@
+﻿import java.util.Scanner;
+public class ex2 {
+public static void main(String args[]) { Scanner input = new Scanner(System.in); int s = input.nextInt();
+int[] array = new int[s]; for(int i=0; i<s; i++) { array[i] = input.nextInt();
+}
+for(int i=0; i<s; i++) {
+System.out.print(array[i] + " ");
+}
