9d424a2a0a138af1c0ce0af9de141c9394f78461
Srimeenakshi KS <meenakshi.ks2022@vitstudent.ac.in>
2024-07-26 10:01:42 +0530

    Create string_comparison.java
    Prompt the user to enter two string inputs and check there is one occurrence of all the characters in string 1 in string 2.

---
+++ string_comparison.java
@@ -0,0 +1,12 @@
+﻿import java.util.Scanner;
+public class ex2 {
+public static void main(String args[]) { Scanner input = new Scanner(System.in); String a = input.nextLine().toLowerCase(); String b = input.nextLine().toLowerCase(); int flag = 0;
+if (a.length() == b.length()) { for (int i=0; i<a.length(); i++) { for (int j = 0; j < b.length(); j++) { if (a.charAt(i) == b.charAt(j)) { flag += 1;
+} else {
+System.out.println("String length do not match and hence not all elements present
+in one element is present in the other.");
+}
+if (flag == a.length()) { System.out.println("True");
+} else {
+System.out.println("False");
+}
