eed47c935a40fa8784818d3fca74e8c14fb2389d
Srimeenakshi KS <meenakshi.ks2022@vitstudent.ac.in>
2024-07-26 09:49:51 +0530

    Create guess_the_number.java
    Generate an integer random number between 1 to 10. Prompt the user to enter a number continuously until the number matches the randomly generated number. For each user input, the program tells the user whether the input is too low or too high, so the user can make the next guess accordingly.

---
+++ guess_the_number.java
@@ -0,0 +1,19 @@
+﻿import java.util.Scanner;
+public class ex2 {
+public static void main(String args[]) {
+Scanner input = new Scanner(System.in); int min = 0;
+int max = 100;
+int a = (int)(Math.random()*(max-min+1)+min); System.out.println("Guess the number!"); Double n = input.nextDouble();
+if (n!= a) {
+while (a != n) {
+System.out.println("That's not the number.."); if (n> a){
+}
+System.out.println("Your answer is greater than the actual number!");
+else if (n <a) {
+}
+System.out.println("Your answer is lower than the actual number!");
+System.out.println("Guess the number!");
+Double m = input.nextDouble();
+n=m;
+}
+System.out.println("That's the correct answer!");
