77daddcee402e2cbc5a706a21a59843bd1a1ec17
Srimeenakshi KS <meenakshi.ks2022@vitstudent.ac.in>
2024-07-26 09:28:30 +0530

    Create greatest_number.java
    Find the greatest number out of the given sequence of numbers.

---
+++ greatest_number_from_given_sequence.java
@@ -0,0 +1,10 @@
+﻿import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in); int n = input.nextInt();
+int a = 0; do {
+int b = input.nextInt(); if (b> a) { a = b; n-;
+}
+} while (n> 1);
+System.out.println("The greatest number is " + a);
+}
