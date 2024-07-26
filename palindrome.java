ecf43e5d9af8edfb1d4010e709a32163d6ec9550
Srimeenakshi KS <meenakshi.ks2022@vitstudent.ac.in>
2024-07-26 09:32:32 +0530

    Create palindrome.java
    Prompt the user to input a string and check if it is a palindrome.

---
+++ palindrome.java
@@ -0,0 +1,19 @@
+﻿import java.util.Scanner;
+public class Main {
+public static void main(String[] args) { Scanner input = new Scanner(System.in); String str = input.nextLine();
+str = str.toLowerCase();
+boolean flag = true;
+int i=0,j= str.length()-1; while (i <j) {
+if (str.charAt(i) != str.charAt(j)) { flag = false; break;
+}
+i++;
+-
+flag = true;
+}
+if (flag) {
+System.out.print("Palindrome");
+} else {
+System.out.print("Not a Palindrome");
+}
+}
+}
