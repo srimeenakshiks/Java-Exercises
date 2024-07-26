99af867ef7b28debf7689a54aeb6d4caa01bc9f7
Srimeenakshi KS <meenakshi.ks2022@vitstudent.ac.in>
2024-07-26 10:10:28 +0530

    Create shuffle_the_given_array.java
    Prompt the user to enter an integer array input and shuffle the given array.

---
+++ shuffle_the_given_array.java
@@ -0,0 +1,15 @@
+﻿import java.util.Scanner;
+public class ex2 {
+public static void main(String args[]) {
+Scanner input = new Scanner(System.in); int n = input.nextint();
+int[] array = new int[n];
+for (int i = 0; i < n; i++) { array[i] = input.nextInt();
+}
+for (int i = 0; i < n; i++) {
+}
+int c = (int) (Math.random() * (n-0 + 1) + 0); int t = array[i];
+array[i] = array[c];
+array[c] = t;
+for (int i = 0; i < n; i++) {
+System.out.print(array[i] + " ");
+}
